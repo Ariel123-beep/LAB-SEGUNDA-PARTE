b8fe7db5ab01347512da77c60557b113318d149d
Ariel123-beep <ariel1234alas@outlook.com>
2021-08-30 17:10:18 -0600

    Actualizacion

---
+++ LAB SEGUNDA PARTE/LAB SEGUNDA PARTE/Entidades/PERSONA.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_SEGUNDA_PARTE.Entidades
+{
+    class PERSONA
+    {
+        private string Nombre;
+        private string Edad;
+        private string Sexo;
+        private int Peso;
+        private int Altura;
+
+        public string Nombre1 { get => Nombre; set => Nombre = value; }
+        public string Edad1 { get => Edad; set => Edad = value; }
+        public string Sexo1 { get => Sexo; set => Sexo = value; }
+        public int Peso1 { get => Peso; set => Peso = value; }
+        public int Altura1 { get => Altura; set => Altura = value; }
+    }
+}
