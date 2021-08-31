8a5da0a634c0aaa12598f1010d18aeda082de1a8
Alvin0183 <alvinmartinez666@outlook.com>
2021-08-30 19:09:11 -0600

    Actualizacion

--- LAB SEGUNDA PARTE/LAB SEGUNDA PARTE/Entidades/PERSONA.cs
+++ LAB SEGUNDA PARTE/LAB SEGUNDA PARTE/Entidades/PERSONA.cs
@@ -19,5 +19,40 @@ namespace LAB_SEGUNDA_PARTE.Entidades
         public string Sexo1 { get => Sexo; set => Sexo = value; }
         public int Peso1 { get => Peso; set => Peso = value; }
         public int Altura1 { get => Altura; set => Altura = value; }
+
+        public PERSONA(string nombre, string edad, string sexo, int peso, int altura)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Sexo = sexo;
+            Peso = peso;
+            Altura = altura;
+        }
+
+        public PERSONA(string nombre, string edad, string sexo)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Sexo = sexo;
+        }
+
+        public PERSONA(string nombre, string edad, string sexo, int peso, int altura, string nombre1, string edad1, string sexo1, int peso1, int altura1) : this(nombre, edad, sexo, peso, altura)
+        {
+            Nombre1 = nombre1;
+            Edad1 = edad1;
+            Sexo1 = sexo1;
+            Peso1 = peso1;
+            Altura1 = altura1;
+        }
+
+        
+
+
+
+
     }
+
+
+
 }
+
---
+++ LAB SEGUNDA PARTE/LAB SEGUNDA PARTE/Negocio/ClsPersona.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_SEGUNDA_PARTE.Negocio
+{
+    class ClsPersona
+    {
+
+            public ClsPersona(string nombre, int edad, string sexo, int peso, double altura)
+            {
+                calcularIMC(peso, altura);
+                esMayorDeEdad(edad);
+            }
+            public static void calcularIMC(int peso, double altura)
+            {
+                double calcularPeso;
+
+                calcularPeso = peso / (altura * altura);
+                if (calcularPeso < 18)
+                {
+                    calcularPeso = -1;
+
+                }
+                if (calcularPeso >= 18 && calcularPeso <= 24.9)
+                {
+                    calcularPeso = 0;
+                }
+
+                if (calcularPeso == -1)
+                {
+                    Console.WriteLine("Tienes muy poco peso para tu estatura");
+                }
+                if (calcularPeso == 0)
+                {
+                    Console.WriteLine("Tienes peso normal para tu estatura");
+                }
+                else
+                {
+                    Console.WriteLine("Tienes mucho peso para tu estatura");
+                }
+
+
+            }
+            public void esMayorDeEdad(int edad)
+            {
+                bool mayor;
+                if (edad >= 18)
+                {
+
+                    mayor = true;
+                }
+                else
+                {
+                    mayor = false;
+                }
+                Console.WriteLine("Soy mayor?: " + mayor);
+            }
+
+            private void comprobarSexo(char sexo)
+            {
+                if (sexo != 'H' || sexo != 'M')
+                {
+                    sexo = 'H';
+                }
+                Console.WriteLine(sexo);
+            }
+
+            public void toString()
+            {
+
+            }
+
+            private static void generaDNI()
+            {
+                int DNI;
+                int resultadoA;
+                int resultadoB;
+                int resultado;
+                string[] letras = new string[23] { "T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E" };
+                Random r = new Random();
+                DNI = r.Next(99999999);
+                resultadoA = DNI / 23;
+                resultadoB = resultadoA * 23;
+                resultado = DNI - resultadoB;
+                Console.WriteLine(DNI + "-" + letras[resultado]);
+
+            }
+    
+
+
+
+    }
+}
